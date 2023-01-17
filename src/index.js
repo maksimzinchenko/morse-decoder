f55316ae132453b5fa3eee3bb1b4af67597211c8
lipkij <lipkij@gmail.com>
2023-01-17 06:39:21 +0400

    solution 2

--- src/index.js
+++ src/index.js
@@ -38,6 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    BIN_ALPHABET = {}
+    for (letter in MORSE_TABLE){
+        bin_letter = [];
+        for (s of letter.split('')){
+            if (s === '.'){
+                bin_letter.push('10');
+            } else {
+                bin_letter.push('11');
+            }
+        }
+        key = bin_letter.join('').padStart(10, '0');
+        BIN_ALPHABET[key] = MORSE_TABLE[letter];
+    }
+    
+    phrase = [];
+    for (i = 0; i < expr.length; i += 10){
+        if (expr[i] == '*'){
+            phrase.push(' ');
+        } else {
+            phrase.push(BIN_ALPHABET[expr.slice(i, i+10)]);
+        }
+    }
+    return phrase.join('');
+}
+
+
+
+
+function decode2(expr) {
     phrase = [];
     for (i = 0; i < expr.length; i += 10){
         letter = expr.slice(i, i + 10).replaceAll('00','').replaceAll('10', '.').replaceAll('11','-');
@@ -49,6 +78,7 @@ function decode(expr) {
     }
     return phrase.join('');
 }
+
 
 module.exports = {
     decode
